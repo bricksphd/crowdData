86a61387b9f98bfe5e327fcd420863d69ae21168
B. Ricks <bricks@unomaha.edu>
2017-01-27 13:19:15 -0600

    Convert a crowd arrival spec into individual arrival times.

--- generator.js
+++ generator.js
@@ -1,19 +1,32 @@
-function convert(){
-  let startingString = document.getElementById("startingJSON").value;
-  let spec = JSON.parse(startingString);
+function convert() {
+    let startingString = document.getElementById("startingJSON").value;
+    let spec = JSON.parse(startingString);
 
-  let agents = [];
+    let agents = [];
 
-  for(let i = 0; i < spec.length; i++)
-  {
-    let spawner = spec[i].spawn;
-    let location = spec[i].location;
+    for (let i = 0; i < spec.length; i++) {
+        let spawner = spec[i].spawn;
+        let location = spec[i].location;
 
-      for(let time = spawner.start; time < spawner.stop; time+=rate){
-        agents.add({"location" : location, "destination": {"x": 0, "y" : 0}, "startTime" : time});
-      }
-  }
+        let otherLocations = [];
 
-  let result = JSON.stringify(agents);
-  document.getElementById("createdJSON").value = result;
+        for (let otherIndex = 0; otherIndex < spec.length; otherIndex++) {
+            if (otherIndex != i) {
+                otherLocations.push(spec[otherIndex]);
+            }
+        }
+
+        for (let time = parseFloat(spawner.start); time <= parseFloat(spawner.stop); time += parseFloat(spawner.rate)) {
+            agents.push({"location": location, "destination": rand(otherLocations).location, "startTime": time});
+        }
+    }
+
+    let result = JSON.stringify(agents);
+    document.getElementById("createdJSON").value = result;
+}
+
+//Based on http://stackoverflow.com/questions/5915096/get-random-item-from-javascript-array
+function rand(array) {
+    let toReturn = array[Math.floor(Math.random() * array.length)];
+    return toReturn;
 }
